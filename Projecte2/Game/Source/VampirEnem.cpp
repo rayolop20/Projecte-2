04b3d350b2692fd41dff8f4c9b0d5c1c87b535a3
Pol Maresch <polmaresch@gmail.com>
2022-03-02 11:56:36 +0100

    Update VampirEnem.cpp

--- Projecte2/Game/Source/VampirEnem.cpp
+++ Projecte2/Game/Source/VampirEnem.cpp
@@ -1,4 +1,37 @@
-#include "Entity.h"
+#include "EntityManager.h"
 #include "VampirEnem.h"
 #include "Collisions.h"
 
+VampirEnem::VampirEnem()
+{
+	name.Create("VampirEnem");
+
+}
+
+VampirEnem::~VampirEnem()
+{
+}
+
+bool VampirEnem::Awake(pugi::xml_node&)
+{
+	return false;
+}
+
+bool VampirEnem::Start()
+{
+	return false;
+}
+
+bool VampirEnem::Update(float dt)
+{
+	return false;
+}
+
+bool VampirEnem::PostUpdate()
+{
+	return false;
+}
+
+void VampirEnem::OnCollision(Collider* c1, Collider* c2)
+{
+}
