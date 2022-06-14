c391a3f0f2b6f6dd8c1cc7fdf824f3613f45ec74
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-06-15 00:54:43 +0200

    Revert "posible revert"
    This reverts commit e3ebb5216a0346b17ee01bdb217434ba5ae52bd2.

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -354,7 +354,7 @@ bool Player::Update(float dt)
 			}
 			//left
 			{
-				if (((app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) && P1.moveXD == false && app->BTSystem->battle == false && P1.IsAlive == true))
+				if (((app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_RIGHT) == KEY_REPEAT) || (app->input->Pad->l_x > 0.5)) && P1.moveXD == false && app->BTSystem->battle == false && P1.IsAlive == true)
 				{
 					P1.position.x += 3;
 					P1.moveXA = true;
@@ -375,7 +375,7 @@ bool Player::Update(float dt)
 					}
 				}
 
-				else if (((app->input->GetKey(SDL_SCANCODE_D) == KEY_UP) && P1.IsAlive == true))
+				else if (((app->input->GetKey(SDL_SCANCODE_D) == KEY_UP) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_RIGHT) == KEY_UP) || (app->input->Pad->l_x > 0 && app->input->Pad->l_x < 0.5)) && P1.IsAlive == true)
 				{
 				
 					P1.moveXA = false;
@@ -394,7 +394,7 @@ bool Player::Update(float dt)
 			}
 			//right
 			{
-				if (((app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) && P1.moveXA == false && app->BTSystem->battle == false && P1.IsAlive == true))
+				if (((app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_LEFT) == KEY_REPEAT) || (app->input->Pad->l_x < -0.5)) && P1.moveXA == false && app->BTSystem->battle == false && P1.IsAlive == true)
 				{
 					P1.position.x -= 3;
 					P1.moveXD = true;
@@ -415,7 +415,7 @@ bool Player::Update(float dt)
 					}
 				}
 
-				else if (((app->input->GetKey(SDL_SCANCODE_A) == KEY_UP)  && P1.IsAlive == true))
+				else if (((app->input->GetKey(SDL_SCANCODE_A) == KEY_UP) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_LEFT) == KEY_UP) || (app->input->Pad->l_x < 0 && app->input->Pad->l_x > -0.5)) && P1.IsAlive == true)
 				{
 					P1.moveXD = false;
 					currentAnim1 = &idleAnim1;
@@ -432,7 +432,7 @@ bool Player::Update(float dt)
 			}
 			//up
 			{
-				if (((app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) && app->BTSystem->battle == false && P1.IsAlive == true))
+				if (((app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_UP) == KEY_REPEAT) || (app->input->Pad->l_y < -0.5)) && P1.moveYW == false && app->BTSystem->battle == false && P1.IsAlive == true)
 				{
 					P1.position.y -= 3;
 					P1.moveYS = true;
@@ -451,7 +451,7 @@ bool Player::Update(float dt)
 
 				}
 
-				else if (((app->input->GetKey(SDL_SCANCODE_W) == KEY_UP) && P1.IsAlive == true))
+				else if (((app->input->GetKey(SDL_SCANCODE_W) == KEY_UP) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_UP) == KEY_UP) || (app->input->Pad->l_y < 0 && app->input->Pad->l_y > -0.5)) && P1.IsAlive == true)
 				{
 					P1.moveYS = false;
 					currentAnim1 = &upAnim1;
@@ -468,7 +468,7 @@ bool Player::Update(float dt)
 			}
 			//down
 			{
-				if (((app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) && P1.moveYS == false && app->BTSystem->battle == false && P1.IsAlive == true))
+				if (((app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_DOWN) == KEY_REPEAT) || (app->input->Pad->l_y > 0.5)) && P1.moveYS == false && app->BTSystem->battle == false && P1.IsAlive == true)
 				{
 					P1.position.y += 3;
 					P1.moveYW = true;
@@ -487,7 +487,7 @@ bool Player::Update(float dt)
 					
 				}
 
-				else if (((app->input->GetKey(SDL_SCANCODE_S) == KEY_UP) && P1.IsAlive == true))
+				else if (((app->input->GetKey(SDL_SCANCODE_S) == KEY_UP) || (app->input->Pad->GetButton(SDL_CONTROLLER_BUTTON_DPAD_DOWN) == KEY_UP) || (app->input->Pad->l_y > 0 && app->input->Pad->l_y < 0.5)) && P1.IsAlive == true)
 				{
 					P1.moveYW = false;
 					currentAnim1 = &idleAnim1;
